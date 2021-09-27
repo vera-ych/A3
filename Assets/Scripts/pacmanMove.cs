efda875e9076774ccb52f6650a1dde9f05bf2364
vera-ych <13054933@student.uts.edu.au>
2021-09-27 11:26:56 +0800

    A

--- Assets/Scripts/pacmanMove.cs
+++ Assets/Scripts/pacmanMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pacman: MonoBehaviour
+public class pacmanMove: MonoBehaviour
 {
     [SerializeField] private Animator pacman;
     private Vector3 endPos;
@@ -14,7 +14,7 @@ public class Pacman: MonoBehaviour
         move = 0;
         endPos = pacman.transform.position;
         tweener = GetComponent<Tweener>();
-        //tweener.AddTween(pacman.transform, pacman.transform.position, pacman.transform.position, 1.5f);
+        tweener.AddTween(pacman.transform, pacman.transform.position, pacman.transform.position, 1.5f);
     }
 
     // Update is called once per frame
@@ -22,31 +22,32 @@ public class Pacman: MonoBehaviour
     {
         if (pacman.transform.position == endPos && move == 0)
         {
-            endPos = new Vector3(-2.8f, 14f, 0f);
+            endPos = new Vector3(9.5f, 0.2f, 0f);
             tweener.AddTween(pacman.transform, pacman.transform.position, endPos, 1.5f);
-            //pacman.transform.Rotate (new Vector3(0f, 0f, -90f));
+            pacman.transform.Rotate(0f, 0f, 0f);
             move = 1;
 
         }
         else if (pacman.transform.position == endPos && move ==1)
         {
-            endPos = new Vector3(-2.8f, 10f, 0f);
+
+            endPos = new Vector3(9.5f, -3.7f, 0f);
             tweener.AddTween(pacman.transform, pacman.transform.position, endPos, 1.5f);
-            //pacman.transform.Rotate(new Vector3(0f, 0f, 90f));
+            pacman.transform.Rotate(180f, 0f, 90f);
             move = 2;
         }
         else if (pacman.transform.position == endPos && move ==2)
         {
-            endPos = new Vector3(-14f, 10f, 0f);
+            endPos = new Vector3(4.45f, -3.7f, 0f);
             tweener.AddTween(pacman.transform, pacman.transform.position, endPos, 1.5f);
-            //pacman.transform.Rotate (new Vector3(0f, 0f, 180f));
+            pacman.transform.Rotate(180f, 0f, 270f);
             move = 3;
         }
         else if (pacman.transform.position == endPos && move ==3)
         {
-            endPos = new Vector3(-14f, 14f, 0f);
+            endPos = new Vector3(4.45f, 0.2f, 0f);
             tweener.AddTween(pacman.transform, pacman.transform.position, endPos, 1.5f);
-            //pacman.transform.Rotate (new Vector3(0f, 0f, 90f));
+            pacman.transform.Rotate(180f, 0f, 90f);
             move = 0;
         }
     }
